774fbaf853fd0cc1e1f3514ab2a5304cd3b456a7
vperez-droid <vperez@dpiestrategia.com>
2025-10-07 10:37:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -178,7 +178,7 @@ else:
         
         if 'gemini_model' not in st.session_state:
             genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-            st.session_state.gemini_model = genai.GenerativeModel('models/gemini-2.5-flash')
+            st.session_state.gemini_model = genai.GenerativeModel('models/gemini-1.5-flash')
 
         model = st.session_state.gemini_model
 
